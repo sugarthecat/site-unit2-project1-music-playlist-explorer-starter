7454d4ddf10415424bca9ffd2c74790c594cc176
Tj Nickerson <tjnickerson@meta.com>
2025-06-04 11:07:31 -0700

    Added images and playlist shuffle

--- music-playlist-creator/Playlist.js
+++ music-playlist-creator/Playlist.js
@@ -1,37 +1,87 @@
 class Playlist {
-  constructor(dataInput) {
-    this.title = dataInput.title;
-    this.creator = dataInput.creator;
+  constructor(title, creator) {
+    this.title = title;
+    this.creator = creator;
     this.dateCreated = new Date();
     this.songs = [];
-    for (let i = 0; i < dataInput.songs.length; i++) {
-      for (let j = 0; j < songs.length; j++) {
-        if (songs[j].title == dataInput.songs[i]) {
-          this.songs.push(songs[j]);
-          break;
-        }
-      }
-    }
   }
   getDOMCard() {
     let ref = this;
 
     let mainBody = document.createElement("div");
-    mainBody.className = "playlist-card"
+    mainBody.className = "playlist-card";
     let playlistCover = document.createElement("img");
-    playlistCover.src = "assets/img/playlist.png"
-    mainBody.appendChild(playlistCover)
+    playlistCover.src = this.getPlaylistCoverImage();
+    mainBody.appendChild(playlistCover);
     let playlistName = document.createElement("h3");
-    playlistName.innerText = this.title
-    mainBody.appendChild(playlistName)
+    playlistName.innerText = this.title;
+    mainBody.appendChild(playlistName);
     let playlistCreator = document.createElement("p");
     playlistCreator.innerText = this.creator;
-    mainBody.appendChild(playlistCreator)
-    mainBody.onclick = function(){ref.focusPlaylist()};
+    mainBody.appendChild(playlistCreator);
+    mainBody.onclick = function () {
+      ref.focusPlaylist();
+    };
     return mainBody;
   }
-  focusPlaylist(){
-    document.getElementById("modal-overlay").style.display = "flex"
-    //TODO: Actually implement
+  focusPlaylist() {
+    document.getElementById("modal-overlay").style.display = "flex";
+    document.getElementById("playlist-name").innerText = this.title;
+    document.getElementById(
+      "playlist-creator-name"
+    ).innerText = `Created by ${this.creator}`;
+    document.getElementById("songs-list").innerHTML = "";
+    for (let i = 0; i < this.songs.length; i++) {
+      document
+        .getElementById("songs-list")
+        .appendChild(this.getSongCard(this.songs[i]));
+    }
+    document.getElementById('playlist-cover').src = this.getPlaylistCoverImage();
+    let ref = this;
+    document.getElementById("shuffleButton").onclick = function () {
+      ref.shuffleSongs();
+    };
+  }
+  getPlaylistCoverImage() {
+    if (this.songs.length == 0) {
+      return "assets/img/playlist.png";
+    } else {
+      return `assets/img/${this.songs[0].albumcover}`;
+    }
+  }
+  getSongCard(song) {
+    let card = document.createElement("div");
+    let image = document.createElement("img");
+    image.src = `assets/img/${song.albumcover}`;
+    card.appendChild(image);
+    let songTitle = document.createElement("p");
+    songTitle.innerText = song.title;
+    songTitle.className = "song-title";
+    card.append(songTitle);
+    let songAlbum = document.createElement("p");
+    songAlbum.innerText = song.album;
+    card.append(songAlbum);
+    let songArtist = document.createElement("p");
+    songArtist.innerText = song.artist;
+    card.append(songArtist);
+    return card;
+  }
+  shuffleSongs() {
+    document.getElementById("songs-list").innerHTML = "";
+    //get random shuffle
+    let indexes = [];
+    while (indexes.length < this.songs.length) {
+      indexes.splice(
+        Math.floor(Math.random() * indexes.length),
+        0,
+        indexes.length
+      );
+    }
+    //insert according to new order
+    for (let i = 0; i < this.songs.length; i++) {
+      document
+        .getElementById("songs-list")
+        .appendChild(this.getSongCard(this.songs[indexes[i]]));
+    }
   }
 }
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,18 +1,34 @@
-function hideModalOverlay(){
-    document.getElementById('modal-overlay').style.display = "none";
+function hideModalOverlay() {
+  document.getElementById("modal-overlay").style.display = "none";
 }
-function modalContentClicked(event){
-    event.stopPropagation();
+function modalContentClicked(event) {
+  event.stopPropagation();
 }
 
-let songs = []
-let playlists = []
+let songs = [];
+let playlists = [];
 //load data
-function loadData(data){
-    songs = data.songs;
-    for(let i = 0; i<data.playlists.length; i++){
-        playlists.push(new Playlist(data.playlists[i]))
-        document.getElementById("playlist-cards").appendChild(playlists[i].getDOMCard())
+function loadData(data) {
+  songs = data.songs;
+  for (let i = 0; i < data.playlists.length; i++) {
+    let playlistData = data.playlists[i];
+    let playlist = new Playlist(playlistData.title, playlistData.creator);
+
+    for (let k = 0; k < playlistData.songs.length; k++) {
+      for (let j = 0; j < songs.length; j++) {
+        if (songs[j].title == playlistData.songs[k]) {
+          playlist.songs.push(songs[j]);
+          break;
+        }
+      }
     }
+    playlists.push(playlist);
+    document
+      .getElementById("playlist-cards")
+      .appendChild(playlists[i].getDOMCard());
+  }
 }
-fetch("assets/data.json").then(x => x.json()).then(x => loadData(x));
+fetch("assets/data.json")
+  .then((x) => x.json())
+  .then((x) => loadData(x));
+hideModalOverlay();
